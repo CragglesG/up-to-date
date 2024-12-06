0fc1b0de08d3d3f283af8988d5c53ece1cc6675b
Craig <121510295+CragglesG@users.noreply.github.com>
2024-12-06 08:42:51 +0000

    Update main.py
    Add post and topic function and more TODOs

--- main.py
+++ main.py
@@ -5,4 +5,20 @@ app = App()
 @app.command("/getmeuptodate")
 def get_me_up_to_date(ack, body, client):
     ack()
-    # TODO: get the user up to date on up to date!
+    # TODO: get the user up to date on Up To Date!
+
+@app.command("/utdpost")
+def post(ack, client):
+    ack()
+    # TODO: Show posting modal
+
+# TODO: Receive posting modal submission
+
+@app.command("/utdtopic")
+def topic(ack, client):
+    ack()
+    # TODO: Show topic modal
+
+# TODO: Receive topic modal submission
+
+# TODO: Implement feed posting mechanism
